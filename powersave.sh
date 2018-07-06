4f1e20683c88afa073b93e96106322885185cc29
Gennadiy Mashentsev <gennas2@gmail.com>
2018-07-07 01:09:44 +0200

    If after wake up battery is still discharging, wait some time and sleep again

--- powersave.sh
+++ powersave.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-SLEEP_TIME=300
+MEM_SLEEP_TIME=300
+MEM_SLEEP_TIME=15
+MEM_TIMEOUT=30
+
+DISK_SLEEP_TIME=600
+DISK_TIMEOUT=30
 
 LOGGER="logger -t powersave"
 $LOGGER "Power save: $@"
 
+function battery_state {
+    echo $(upower -i $(upower -e | grep 'BAT') | grep -E "state" | awk '{print $NF}')
+}
+
 if [[ -n "$1" && "$1" == "true" ]]; then
-    battery_charge=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $NF}')
+    sleep $MEM_TIMEOUT
 
-    $LOGGER "Battery charge: $battery_charge"
+    while [ $(battery_state) = "discharging" ]
+    do
+        $LOGGER "Battery state: $(battery_state)"
 
-    if [ "${battery_charge:0:-1}" -gt 20 ]; then
-        mode="mem"
-    else
-        mode="disk"
-    fi
+        battery_charge=$(upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $NF}')
 
-    $LOGGER "Forcing $mode sleep for $SLEEP_TIME seconds"
+        $LOGGER "Battery charge: $battery_charge"
 
-    rtcwake -m $mode -s $SLEEP_TIME
+        if [ "${battery_charge:0:-1}" -gt 20 ]; then
+            mode="mem"
+            sleep_time=$MEM_SLEEP_TIME
+            timeout=$MEM_TIMEOUT
+        else
+            mode="disk"
+            sleep_time=$DISK_SLEEP_TIME
+            timeout=$DISK_TIMEOUT
+        fi
+
+        $LOGGER "Forcing $mode sleep for $sleep_time seconds"
+        rtcwake -m $mode -s $sleep_time
+        sleep $timeout
+    done
 fi
 
